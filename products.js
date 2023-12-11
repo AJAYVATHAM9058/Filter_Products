5a1ae6757e70d1d5e39eea5195fa948b9c19e3ba
Vatham Ajay <75926757+AJAYVATHAM9058@users.noreply.github.com>
2023-12-12 01:34:08 +0530

    Add files via upload

---
+++ products.js
@@ -0,0 +1,108 @@
+const products = [
+		{
+			id:'hello1',
+			title:'modern bookshelf',
+			company : 'Wipro',
+			image :'https://course-api.com/images/store/product-7.jpeg',
+			price : 8.99,
+		},
+		
+		{
+			id:'hello2',
+			title : 'Mattresses',
+			company :'Maruthi',
+			image : 'https://th.bing.com/th/id/OIP.v5mqFRX8AThK-6Xu1yTqPwHaHa?rs=1&pid=ImgDetMain',
+			price :15.99,
+		},
+		
+		{
+			id:'hello3',
+			title:'Modern Bed',
+			company : 'Tata',
+			image : 'https://i5.walmartimages.com/asr/19eeae4d-dfc4-4fad-9c78-e0d3b8f114ed_1.87cb4d888eab4d02393b3700b171224e.jpeg',
+			price : 99.9,
+		},
+
+		{
+			id:'hello4',
+			title:'Almariah',
+			company : 'Bajaj',
+			image : 'https://th.bing.com/th/id/OIP.61BKXlp_j9ViuwsbPSgftQHaEW?rs=1&pid=ImgDetMain',
+			price : 35.98,
+		},
+
+		{
+			id:'hello5',
+			title:'Scooty',
+			company : 'Wipro',
+			image : 'https://www.carblogindia.com/wp-content/uploads/2017/05/2017-tvs-scooty-110-matte-blue.png',
+			price : 156.99,
+		},
+
+		{
+			id:'hello6',
+			title:'pulsar bike',
+			company : 'Maruthi',
+			image : 'https://i0.wp.com/www.carblogindia.com/wp-content/uploads/2016/12/2017-Bajaj-Pulsar-150-Laser-Black-Side-Profile-Image.jpg',
+			price : 999.99,
+		},
+
+		{
+			id:'hello7',
+			title:'BMW Car',
+			company : 'Tata',
+			image : 'https://th.bing.com/th/id/OIP.RWYggzUNsj4eY85uoHTCwQHaEK?rs=1&pid=ImgDetMain',
+			price : 10000.99,
+		},
+
+		{
+			id:'hello8',
+			title:'Book shelves',
+			company : 'Wipro',
+			image : 'https://th.bing.com/th/id/OIP.M7pOD7BBDod0J1esE-MlIAHaHa?rs=1&pid=ImgDetMain',
+			price : 15.94,
+		},
+
+		{
+			id:'hello9',
+			title:'water heater',
+			company:'Tata',
+			image:'https://th.bing.com/th/id/OIP.fazlsfgA3hN8xtZ4Yu452AHaHX?rs=1&pid=ImgDetMain',
+			price: 19.654
+		},
+
+		{
+			id:'hello10',
+			title:'Room heater',
+			company:'Bajaj',
+			image : 'https://th.bing.com/th/id/OIP.kUwQjTX667pBjWIvl9yS0QHaI3?rs=1&pid=ImgDetMain',
+			price : 65.4,
+		},
+
+		{
+			id:'hello11',
+			title:'washing machine',
+			company:'Wipro',
+			image:'https://th.bing.com/th/id/OIP.MZpkq1HZLpk04c_lRQzgOwHaJL?rs=1&pid=ImgDetMain',
+			price:85.35,
+		},
+
+		{
+			id:'hello12',
+			title:'helicopter',
+			company:'Maruthi',
+			image:'https://th.bing.com/th/id/OIP.HT7rnGp_RH9z8zeZQRZK2AHaE8?rs=1&pid=ImgDetMain',
+			price:99999.66,
+
+		},
+
+		{
+			id:'hello13',
+			title:'Dining table',
+			company:'Bajaj',
+			image:'https://th.bing.com/th/id/OIP.cPOT8q6hgFsuz-sWHHthrQHaHa?rs=1&pid=ImgDetMain',
+			price:452.565,
+		},
+
+
+	];
